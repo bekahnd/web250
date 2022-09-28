910e120874880d51ae6d6f6166a11fdb9c4c04d7
Bekah Downer <bekah2126@gmail.com>
2022-09-27 22:43:08 -0400

    Completed asgn03

--- asgn03.5-bike-challenge/private/classes/bicycle.class.php
+++ asgn03.5-bike-challenge/private/classes/bicycle.class.php
@@ -29,6 +29,7 @@ class Bicycle {
     $this->model = $args['model'] ?? '';
     $this->year = $args['year'] ?? '';
     $this->category = $args['category'] ?? '';
+    $this->color = $args['color'] ?? '';
     $this->description = $args['description'] ?? '';
     $this->gender = $args['gender'] ?? '';
     $this->price = $args['price'] ?? 0;
