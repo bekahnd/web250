5f3f43a339fa84fec2b1471161e1e38ca11c2608
Bekah Downer <bekah2126@gmail.com>
2022-11-15 19:28:10 -0500

    Starting assignment 10

---
+++ sab/private/db_credentials.php
@@ -0,0 +1,16 @@
+<?php
+
+// Local
+define("DB_SERVER", "localhost");
+define("DB_USER", "webuser");
+define("DB_PASS", "P@ssword12345");
+define("DB_NAME", "sabird");
+
+// SiteGround
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "uxvelsvngpl3l");
+// define("DB_PASS", "3*2c1_61V+_~");
+// define("DB_NAME", "db4z9ehlwqjucc");
+
+
+?>
