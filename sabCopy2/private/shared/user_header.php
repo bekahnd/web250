748628a7fe1d09ce1034a965d6b4d6588b6fd17c
Bekah Downer <bekah2126@gmail.com>
2022-11-30 14:34:16 -0500

    Completed asgn11 with working delete bird

---
+++ sabCopy2/private/shared/user_header.php
@@ -0,0 +1,30 @@
+<?php
+  if(!isset($page_title)) { $page_title = 'Bird Area'; }
+?>
+
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>SA Birds - <?php echo h($page_title); ?></title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <header>
+      <h1>SA Birds Admin Area</h1>
+    </header>
+
+    <nav>
+      <ul>
+        <?php if($session->is_logged_in()) { ?>
+        <li>User: <?php echo $session->username; ?></li>
+        <li><a href="<?php echo url_for('/users/index.php'); ?>">Menu</a></li>
+        <li><a href="<?php echo url_for('/logout.php'); ?>">Logout</a></li>
+        <?php } ?>
+      </ul>
+    </nav>
+
+
+
+    <?php echo display_session_message(); ?>
